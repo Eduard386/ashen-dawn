9945e3b244c8bb5a35a93c6a2c8ccd39526f5eff
Eduard Stepanyshchenko <eduardstepanyshchenko@192.168.1.7>
2025-08-01 00:20:03 +0300

    migration

--- dist/legacy/scenes/DeadScene.d.ts
+++
@@ -1,8 +0,0 @@
-export default class DeadScene extends Phaser.Scene {
-    constructor();
-    preload(): void;
-    create(): void;
-    death_music: Phaser.Sound.NoAudioSound | Phaser.Sound.HTML5AudioSound | Phaser.Sound.WebAudioSound;
-    spaceKey: Phaser.Input.Keyboard.Key;
-    update(): void;
-}
